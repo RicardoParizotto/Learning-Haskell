ae237e2ae08f8af0de68e1a0390f1225cd2a123e
Ricardo Parizotto <ricardo.dparizotto@gmail.com>
2017-11-28 16:05:13 -0200

    Update trafficlights.hs

--- homework1/trafficlights.hs
+++ homework1/trafficlights.hs
@@ -1,27 +1,35 @@
 {-# LANGUAGE OverloadedStrings #-}
+
 import CodeWorld
 
+botCircle, topCircle, midCircle :: Color -> Picture
+botCircle c = colored c (translated 0 (-3) (solidCircle 1))
+topCircle c = colored c (translated 0   3  (solidCircle 1))
+midCircle c = colored c (translated 0   0  (solidCircle 1))
 
-midPicture color = colored color (translated 0 (0) (solidCircle 1))
-botPicture color = colored color (translated 0 (-3) (solidCircle 1))
-topPicture color = colored color (translated 0 3 (solidCircle 1))
-
+frame :: Picture
 frame = rectangle 2.5 8.5
 
-
 trafficLight :: Integer -> Picture
+trafficLight 0  = botCircle green & midCircle black & topCircle black & frame
+trafficLight 1 = botCircle black & midCircle yellow & topCircle black & frame
+trafficLight 2= botCircle black & midCircle black & topCircle red & frame
+trafficLight 3 = botCircle black & midCircle yellow & topCircle red & frame
 
-trafficLight 1 = topPicture green & midPicture black & botPicture black & frame
-trafficLight 2 = topPicture black & midPicture yellow & botPicture black & frame
-trafficLight 3 = topPicture black & midPicture black & botPicture red & frame
-trafficLight 4 = topPicture black & midPicture yellow & botPicture red & frame
+
+time :: Double -> Integer
+time t = round(t) `mod` 10
 
 
 trafficController :: Double -> Picture
-trafficController t
-  | round (t/3) `mod` 2 == 0 = trafficLight 1
-  | otherwise                = trafficLight 2
-
-
+trafficController t = 
+        if time t < 5 && time t > 0
+             then trafficLight 0 
+             else if time t == 5
+                 then trafficLight 1
+                 else if time t > 5
+                     then trafficLight 2
+                     else trafficLight 3
+                                                  
 main :: IO ()
 main = animationOf trafficController
